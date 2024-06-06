3b502c135c3526ea648758b875355f13c142d388
samir984 <sasuki984@gmail.com>
2024-06-07 01:14:06 +0545

    implement dropdown menu componenet

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,45 +1,69 @@
 import appLogo from "../assets/Company_logo.png";
-
+import { IoIosClose } from "react-icons/io";
 import { SlArrowDown } from "react-icons/sl";
-import { SlArrowUp } from "react-icons/sl";
 import { IoSearch } from "react-icons/io5";
-import { useState } from "react";
-import { DiVim } from "react-icons/di";
+import { ReactNode, useState } from "react";
 
-export default function Header() {
+import blogIcon from "../assets/blog.png";
+import lightningIcon from "../assets/lightning.png";
+import videoIcon from "../assets/play-circlevideo.png";
+
+export default function Header({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-20 border-b-2 border-white  items-center justify-center  ">
+    <div className="flex h-20 border-b-2 border-white items-center justify-center  ">
       <div className="flex items-center justify-between  w-[97%] tablet:w-[90%] laptop:w-[80%]">
         <div className="flex  items-center gap-8  ">
           <Logo />
           <NavBar />
         </div>
-
-        <div>
-          <Search />
-        </div>
+        <div>{children}</div>
       </div>
     </div>
   );
 }
 
-function Search() {
+export function Search({
+  setDarkBgGround,
+}: {
+  setDarkBgGround: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const [openSearchBox, setOpenSearchBox] = useState(false);
   return (
     <>
       <IoSearch
         color="white"
         size={28}
-        onClick={() => setOpenSearchBox(true)}
+        onClick={() => {
+          setOpenSearchBox(true);
+          setDarkBgGround(true);
+        }}
       />
-      {openSearchBox && (
-        <div className="w-80 relative ">
-          <div className="w-full">
-            <IoSearch color="white" size={28} />
-            <input type="text"  className=""/>
+
+      <div
+        className={`absolute -top-12 right-[50%] translate-x-[50%] transition-all duration-300 ${
+          openSearchBox && "top-32 "
+        }`}
+      >
+        <div className="w-[300px] tablet:w-[500px] relative bg-white rounded-lg ">
+          <div
+            className="absolute right-0 -top-3 bg-white rounded-full"
+            onClick={() => {
+              setOpenSearchBox(false);
+              setDarkBgGround(false);
+            }}
+          >
+            <IoIosClose />
+          </div>
+          <div className="flex  gap-2 w-full p-2">
+            <IoSearch color="black" size={28} />
+            <input
+              type="text"
+              className="w-full outline-none"
+              placeholder="search"
+            />
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
@@ -53,18 +77,79 @@ function Logo() {
 }
 
 function NavBar() {
-  const navItems = ["Home", "About Us", "Case Studies", "Resources"];
+  const navItems = [
+    { label: "Home", key: "home" },
+    { label: "About Us", key: "about-us" },
+    { label: "Case Studies", key: "case-studies" },
+    {
+      label: "Resources",
+      key: "resources",
+      depthLevel: 1,
+      subNavItems: [
+        {
+          icon: blogIcon,
+          label: "Blog",
+          key: "Blog",
+          text: "The latest industy new,updates and info.",
+        },
+        {
+          icon: lightningIcon,
+          label: "Customer stories",
+          key: "customer-stories",
+          text: "Learn how our customers are making big changes.",
+        },
+        {
+          icon: videoIcon,
+          label: "Video tuitorials",
+          key: "video-tuitorials",
+          text: "Get up and running on new features and techniques.",
+        },
+      ],
+    },
+  ];
+
   return (
     <ul className="flex  gap-8">
       {navItems.map((item) => {
         return (
-          <div key={item}>
-            <li className="text-white text-xl font-bold hover:text-green-400 transition-all duration-200">
-              <a href={`#${item}`}>{item}</a>
+          <div key={item.key}>
+            <li
+              className={`text-white text-xl font-inter laptop:font-medium flex items-center gap-2 ${
+                item.label && "group relative"
+              }`}
+            >
+              <a href={`#${item.key}`}>{item.label}</a>
+              {item.depthLevel && <SlArrowDown size={16} />}
+              {item.depthLevel && (
+                <DropDownNav subNavItems={item.subNavItems} />
+              )}
             </li>
           </div>
         );
       })}
     </ul>
+  );
+}
+
+function DropDownNav({ subNavItems }) {
+  return (
+    <div className="absolute hidden  group-hover:flex flex-col  top-7 rounded-lg  w-[412px]">
+      <div className="h-5"></div>
+      <div className="flex  flex-col gap-4 bg-white p-8 ">
+        {subNavItems.map((item) => {
+          return (
+            <div className="flex gap-6 items-baseline " key={item.key}>
+              <img src={item.icon} alt="icon" />
+              <div className="flex flex-col  ">
+                <h4 className="font-semibold font-inter text-lg text-gray-900">
+                  {item.label}
+                </h4>
+                <p className="text-gray-500 text-sm font-normal">{item.text}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
--- src/component/HeroSection.tsx
+++ src/component/HeroSection.tsx
@@ -1,21 +1,26 @@
+import { useState } from "react";
 import heroBgImg from "../assets/hero.jpeg";
-import Header from "./Header";
+import Header, { Search } from "./Header";
 
 export default function HeroSection() {
+  const [darkBgGround, setDarkBgGround] = useState(false);
   return (
     <div
-      className={
-        "w-full h-[728px] relative opacity-90    bg-cover bg-no-repeat "
-      }
+      className={"w-full h-[728px] relative opacity-90 bg-cover bg-no-repeat "}
       style={{
         backgroundImage: `url(${heroBgImg})`,
       }}
     >
+      {darkBgGround && (
+        <div className="absolute top-0 w-full h-full bg-black opacity-60 -z-10"></div>
+      )}
       <div
         className="absolute top-0 w-full h-full bg-gradient-to-b from-blue-900 to-blue-800
-] opacity-70 -z-10"
+] opacity-70 -z-20"
       ></div>
-      <Header />
+      <Header>
+        <Search setDarkBgGround={setDarkBgGround} />
+      </Header>
     </div>
   );
 }
