d91ea3dbfba71fef8d8faf03558f780f6264b96e
samir984 <sasuki984@gmail.com>
2024-06-08 13:42:00 +0545

    add loader when app first mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import AboutUsSection from "./component/AboutUsSection";
 import CampaignSection from "./component/CampaignSection";
 import CompaniesSection from "./component/CompaniesSection";
@@ -9,17 +10,31 @@ import NewSection from "./component/NewSection";
 import ResourceSection from "./component/ResourceSection";
 
 export default function App() {
+  const [appLoading, setAppLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setAppLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
-      <HeroSection />
-      <CampaignSection />
-      <AboutUsSection />
-      <CompaniesSection />
-      <NewSection />
-      <ResourceSection />
-      <ContactFormSection />
-      <Footer />
-      <CopywriteSection />
+      {appLoading ? (
+        <div className=" flex justify-center items-center w-full h-screen ">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <>
+          <HeroSection />
+          <CampaignSection />
+          <AboutUsSection />
+          <CompaniesSection />
+          <NewSection />
+          <ResourceSection />
+          <ContactFormSection />
+          <Footer />
+          <CopywriteSection />
+        </>
+      )}
     </>
   );
 }
